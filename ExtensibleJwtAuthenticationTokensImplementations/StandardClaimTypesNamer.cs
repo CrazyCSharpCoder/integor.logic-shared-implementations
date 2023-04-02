2dd02977161e65ff10899b85415bc3f320964821
Stephen L <logunov.stepan2001@gmail.com>
2023-04-03 00:59:39 +0700

    Impelent claim namings

---
+++ ExtensibleJwtAuthenticationTokensImplementations/StandardClaimTypesNamer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using ExtensibleRefreshJwtAuthentication;
+
+namespace ExtensibleJwtAuthenticationTokensImplementations
+{
+	public class StandardClaimTypesNamer : IClaimTypesNamer
+	{
+		private const string _username = "Username";
+		private const string _userId = "UserId";
+
+		public string UsernameClaimType => _username;
+		public string UserRoleClaimType => _userId;
+	}
+}
